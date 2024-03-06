9154a1a64616a6e9114f1b3e709a78fc5397cd90
Toby <tobytietphoto@gmail.com>
2024-03-05 19:17:50 -0800

    made function for player selection.

--- javascript.js
+++ javascript.js
@@ -1,56 +1,42 @@
-let choices = ["rock", "paper", "scissors"]
-// An array that contains 3 strings - rock paper scissors
+let pick = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
- const random = Math.floor(Math.random() * choices.length);
- console.log(choices[random]);
-}
-/* A function that contains a variable that will round down with
-.random method. The .random method only uses number between 0 to 1
-which include decimals. The index of the variable choices will 
-multiply with .random numbers so this will result between 0 to 2.
-*/
-console.log(getComputerChoice())
-// The result will then get loged onto console.
+ const random = Math.floor(Math.random() * pick.length);
+ console.log(pick[random]);
+};
 
-let getPlayerChoice = prompt("Pick one: Rock Paper Scissor");
-// prompt for player's input.
+let choice = prompt("Pick one: rock paper scissor").toLowerCase()
 
-let playerChoiceLowerCase = getPlayerChoice.toLowerCase();
-// variable to convert prompt answer to lowercase.
+    if  (choice === "rock") {
+        console.log("rock");
+        } else if (choice === "paper") {
+        console.log("paper");
+        } else if (choice === "scissors") {
+        console.log("scissor");
+        } else {
+        console.log("invalid");
+        }
 
-console.log(playerChoiceLowerCase)
-// prints into console.
-
-function playerChoice(choice) {
-    if (choice === "rock") {
-        return "rock";
-    } else if (choice === "paper") {
-        return "paper";
-    } else if (choice === "scissors") {
-        return "scissor";
-    } else {
-        return "invalid";
+function game(choice, getComputerChoice) {
+    if ("rock" && "rock") {
+        alert("Tie!");
+    } else if ("rock" && "paper") {
+        alert("Sorry, you loss!");
+    } else if ("rock" && "scissor") {
+        alert("Congrats! you win!");
+    } else if ("paper" && "rock") {
+        alert("Congrats! you win!");
+    } else if ("paper" && "paper") {
+        alert("Tie!");
+    } else if ("paper" && "scissor") {
+        alert("Sorry, you loss!");
+    } else if ("scissor" && "rock") {
+        alert("Congrats! You win!");
+    } else if ("scissor" && "paper") {
+        alert("Sorry, you loss!");
+    } else if ("scissor" && "scissor") {
+        alert("Tie");
     }
 }
 
-console.log(playerChoice())
-
-/* A variable that will bring up a prompt so user can pick between
-rock, paper, scissors.
-*/
-//let playerChoiceLowerCase = getPlayerChoice.toLowerCase();
-/*
-function game(playerChoiceLowerCase, computerChoice) {
-    if ("rock" && "scissors") {
-        return "I win!"
-    } else if ("rock" && "rock") {
-        return "It's a tie!"
-    } else if ("rock" && "paper") {
-        return "Computer wins!"
-    } else if ("scissors" && "scissors") {
-        return "It's a tie!"
-    } else ("paper" && "paper")
-        return "It's a tie!"
-};
-*/
+console.log(game(choice, getComputerChoice))
