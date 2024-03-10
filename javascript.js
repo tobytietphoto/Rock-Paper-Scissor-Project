8af44ec7e258c59ebf614f159f544a2fedd7bdf3
Toby <tobytietphoto@gmail.com>
2024-03-10 14:31:13 -0700

    drastically adjusted the javascript code by adjusting userprompt selection. I fixed the game function and made a function that played the round five consecutively times.

--- javascript.js
+++ javascript.js
@@ -1,63 +1,85 @@
-choices = ["rock", "paper", "scissor"];
+choices = ["rock", "paper", "scissor"]
 
 function getComputerChoice() {
-const random = Math.floor(Math.random() * choices.length)
-return(choices[random]);
+    const random = Math.floor(Math.random() * choices.length)
+    return(choices[random])
 }
 
-//console.log(getComputerChoice())
+function playerPrompt() {
+    let selection = prompt("Pick one: rock, paper, scissor:")
+    selection = selection.toLowerCase();
+    if (selection == null) {
+        return playerPrompt(selection);
+    } else if (!choices.includes(selection)) {
+        return playerPrompt(selection);
+    }
+    return selection;
+}
 
-let playerPrompt = prompt("Select one: rock, paper, scissor");
+//console.log(playerPrompt())
+//Dear lordy jesus christ it works.
 
-let selection = playerPrompt.toLowerCase()
+function checkWinner(a, b) {
+    if(a == b){
+    return `tie`;
+    } 
+    else if (
+        (a == "rock" && b == "scissor") ||
+        (a == "paper" && b == "rock") ||
+        (a == "scissor" && b == "paper")
+    ){
+        return "player";
+    }
+    else {
+        return "computer";
+    }
+}
+/*
+let player = playerPrompt();
+let computer = getComputerChoice();
+ console.log(checkWinner(player, computer))
+ Thank you Jesus! this worked!
+ */
 
-function getPlayerSelection(selection) {
-    if (selection === "rock") {
-            return("rock");
-            } else if (selection === "paper") {
-            return("paper");
-            } else if (selection === "scissor") {
-            return("scissor");
-            } else {
-            return("invalid");
-            }
-        }
-
-console.log(getPlayerSelection(selection))
-
+let playerScore = 0;
+let compScore = 0;
+let playerSelection = playerPrompt();
 let computerSelection = getComputerChoice();
-//console.log(computerSelection)
-// Tested! it works!
 
-let playerSelection = selection;
-//console.log(playerSelection)
-// Tested. It also works!
+function playRound() {
+    let result = checkWinner(playerSelection, computerSelection);
+    if (result == "tie") {
+        return "It's a tie!"
+    } else if (result == "player") {
+        playerScore++
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else if (result == "computer") {
+        compScore++
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    } else {
+        return "invalid"
+    }
+}
+/*
+console.log(playRound())
+This works too TT_TT
+*/
 
-function playRound(a, b) {
-    if (a == "rock" && b == "rock") {
-    return "tie!";
-    } else if (a == "rock" && b == "paper") {
-    return "You lose!";
-    } else if (a == "rock" && b == "scissor") {
-    return "You win!";
-    } else if (a == "paper" && b == "rock") {
-    return "You win!";
-    } else if (a == "paper" && b == "paper") {
-    return "Tie!"
-    } else if (a == "paper" && b == "scissor") {
-    return "You Lose!"
-    } else if (a == "scissor" && b == "rock") {
-    return "You lose!"
-    } else if (a == "scissor" && b == "paper") {
-    return "You win!"
-    } else if (a == "scissor" && b == "scissor") {   
-    return "Tie!" 
-    } else if (a == "invalid" && b == "rock") {
-    return "Not valid"       
-    } else if (a =="invalid" && b == "paper") {
-    return "Not valid" 
-    } else (a=="invalid" && b == "scissors") 
-    return "Not valid" 
+
+function playToFive() {
+    for (let round = 0; round <= 5; round++) {
+    playerSelection = playerPrompt();
+    computerSelection = getComputerChoice()
+    console.log(playRound(playerSelection, computerSelection))
+    }
+    if (playerScore > compScore) {
+        console.log("player wins with " + playerScore + " points")
+    } else if (compScore > playerScore) {
+        console.log("computer wins with " + compScore + " points")
+    } else {
+        console.log("its a tie!")
+    }
+
 }
 
-console.log(playRound(playerSelection, computerSelection))
+console.log(playToFive())
